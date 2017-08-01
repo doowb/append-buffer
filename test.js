66e3fcb8d83204766f3f2fff929ef8d247b28c07
doowb <brian.woodward@gmail.com>
2017-08-01 12:01:20 -0400

    implementation
     - bring in code from file-normalize (original introduced in PR https://github.com/jonschlinkert/file-normalize/pull/3)
     - add tests

---
+++ test.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var os = require('os');
+require('mocha');
+var assert = require('assert');
+var appendBuffer = require('./');
+
+describe('appendBuffer', function() {
+  it('should append the buffer suffix to the buffer prefix with \\r\\n', function() {
+    assert.deepEqual(appendBuffer(new Buffer('abc\r\n'), new Buffer('def')), new Buffer('abc\r\ndef\r\n'));
+  });
+
+  it('should append the buffer suffix to the buffer prefix with \\n:', function() {
+    assert.deepEqual(appendBuffer(new Buffer('abc\n'), new Buffer('def')), new Buffer('abc\ndef\n'));
+  });
+
+  it('should append the buffer suffix to the buffer prefix without EOL', function() {
+    assert.deepEqual(appendBuffer(new Buffer('abc'), new Buffer('def')), new Buffer('abc' + os.EOL + 'def'));
+  });
+
+  it('should append the string suffix to the buffer prefix with \\r\\n', function() {
+    assert.deepEqual(appendBuffer(new Buffer('abc\r\n'), 'def'), new Buffer('abc\r\ndef\r\n'));
+  });
+
+  it('should append the string suffix to the buffer prefix with \\n:', function() {
+    assert.deepEqual(appendBuffer(new Buffer('abc\n'), 'def'), new Buffer('abc\ndef\n'));
+  });
+
+  it('should append the string suffix to the buffer prefix without EOL', function() {
+    assert.deepEqual(appendBuffer(new Buffer('abc'), 'def'), new Buffer('abc' + os.EOL + 'def'));
+  });
+});
